5e74f53afc72a8c289bb547bb8efa0edc61abe18
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-07 16:56:50 +0600

    model form created

--- first_app/forms.py
+++ first_app/forms.py
@@ -5,3 +5,16 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = StudentModel
         fields = '__all__'
+        labels = {
+            'name': 'Student Name',
+            'roll': 'Student Roll',
+        }
+        # widgets = {
+        #     'name': forms.TextInput(attrs={'class': 'btn btn-primary'})
+        # }
+        help_texts = {
+            'name': "Write your full name"
+        }
+        error_messages = {
+            'name': {'required': 'Your name is required'}
+        }
